6acb0563b923950be6c6a44637c5bd777a39ad39
Sadique Iqbal <sadiqueiqbal.si@gmail.com>
2023-01-31 14:29:24 +0530

    thanks IvanBoiko92

--- src/main/java/com/epam/rd/autotasks/GraduallyDecreasingCarousel.java
+++ src/main/java/com/epam/rd/autotasks/GraduallyDecreasingCarousel.java
@@ -4,17 +4,73 @@ public class GraduallyDecreasingCarousel extends DecrementingCarousel {
     public GraduallyDecreasingCarousel(final int capacity) {
         super(capacity);
     }
-
-   /* @Override
-    public boolean addElement(int element) {
-        if (element > 0 && index < capacity && !isRun) {
-            carousel[index++] = element;
-            return true;
+    @Override
+    public CarouselRun run() {
+        if (!isRun) {
+            isRun = true;
+            return new GraduallyRun();
         }
-        return false;
-    }*/
+        return null;
+    }
+}
+class GraduallyRun extends CarouselRun {
+    int decrement = 1;
 
     @Override
+    public int next() {
+        int beforeDecreasing;
+        if (isFinished())
+            return -1;
+        else {
+            beforeDecreasing = array[position];
+            array[position] -= decrement;
+            do {
+                position++;
+                if (position == array.length) {
+                    decrement++;
+                    position = 0;
+                }
+            } while ((array[position] <= 0) && !isFinished());
+        }
+        return beforeDecreasing;
+    }
+}
+
+/*class GraduallyDecreasingCarouselRun extends CarouselRun {
+
+    public int decrementCounter = 1;
+
+    @Override
+    public int next() {
+        int currentValue;
+        if (isFinished()) {
+            return -1;
+        } else {
+            currentValue = array[position];
+            int decrementedValue = currentValue - decrementCounter;
+            if (decrementedValue <= 0) {
+                array[position] = 0;
+                decrementCounter = 1;
+            } else {
+                array[position] = decrementedValue;
+                decrementCounter++;
+            }
+            position++;
+            if (position == array.length) {
+                position = 0;
+            }
+            while (array[position % array.length] <= 0 && !isFinished()) {
+                position++;
+                if (position == array.length) {
+                    position = 0;
+                }
+            }
+        }
+        return currentValue;
+    }
+}
+
+  /*  @Override
     public CarouselRun run() {
         if (!isRun) {
             isRun = true;
